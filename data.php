3789874f3205ff973b1dec59deef9fef546d5058
rezaandika142 <70564413+rezaandika142@users.noreply.github.com>
2020-09-16 14:57:36 +0700

    index.php

---
+++ data.php
@@ -0,0 +1,40 @@
+<?php 
+
+$data_siswa=[
+    "reyhan"=>["Muhammad Reyhan Rizki Ferdiyan","Bondowoso",24/05/2001,True],
+    "matsna"=>["Matsna Hidayatur Rochman","Magelang",01/05/2004,false],
+    "bayu"=>["Raditya Fajar Bayu Pamungkas","Kendal",19/02/2003,false],
+    "rimba"=>["Muhammad Rimba Abdillah","Jepara",02/07/2004,false],
+    "udin"=>["Khoirudin","Sukorejo","05/09/2003",false],
+    "rezafh"=>["Reza Fatikhul Hidayat","Sukorejo","06/08/2004",false],
+    "toba"=>["Toba Fathir Auladya","Kendal",04/04/2005,false],
+    "anang"=>["Anang Syah Amirul Haqim","Jepara",20/10/2003,false],
+    "miftah"=>["Miftachul Rohman","Magelang","08/10/2003",false],
+    "rezamupat"=>["Reza Andika Pratama","Sukorejo","08/06/2004",false],
+    "rezaap"=>["Reza Aditya Pratama","Jepara",04/04/2004,false],
+    "huda"=>["ahmad khaniful huda","Magelang",21/04/2002,false],
+    "ferdi"=>["muhammad ferdiansyah","Jepara",14/07/2003,false],
+    "rendi"=>["Rendi saputra","Magelang",01/04/2004,false],
+    "yosafat"=>["Yosafat Restu Adi","Jepara","06/09/2003",false],
+    "akbar"=>["muhammad akbar kurnia","Sukorejo",07/03/2004,false],
+    "yuda"=>["kusuma yuda mubarok","Sukorejo",25/07/2004,false],
+    "yono"=>["sudiyono","Kendal",10/05/2004,false],
+    "dellas"=>["aodellas magosiang","Semarang",14/03/2004,false],
+    "munif"=>["Munif soleh","Magelang",18/01/2004,false],
+    "zenu"=>["Muhammad Zainuddin Basyar","Jepara","30/09/2003",false],
+ ];
+
+//  foreach($data_siswa as $key => $value){
+//      if($value[3]){
+//          echo "$value[0] \t sudah lulus \n";
+//      }
+//      else{
+//          echo "$value[0] \t belum lulus \n";
+//      }
+//  }
+
+$data_slider=["asset/images/slider/slider-1.jpg","asset/images/slider/irm.jpg","asset/images/slider/slider-3.jpg"];
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,40 +1,71 @@
-<?php include "data.php"> ?>
+<?php include "data.php"; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Siswa Magang TOP</title>
-    <link rel="stylesheet" href="asset/css/boostrap.min.css">
-    <link rel="stylesheet" href="asset/css/all.css">
-    <link rel="shortcut icon" href="asset/images/icon.ico">
+    <title>Document</title>
+   <link rel="stylesheet" href="asset/css/boostrap.min.css">
+   <link rel="stylesheet" href="asset/css/all.css">
 </head>
 <body>
-     <div class="container">
 
-         <div class="row">
-            
-            <?php foreach($data_siswa as $k=>$v );?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+              
+                    <?php $nomor=1; foreach($data_slider as $data) : ?>
+                    <div class="carousel-item <?php if($nomor==1){echo "active";}?>">
+                        <img src="<?php echo $data; ?>" class="d-block w-100" alt="logo-<?php echo $nomor; ?>">
+                    </div>  
+             
+                        <?php $nomor++; endforeach; ?>
+             
+                     </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>  
+    </div>
+
+
+
+
+      <div class="container">
+        <div class="row">
+
+            <?php foreach($data_siswa as $key => $value): ?>
 
             <div class="col-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="asset/images/logo.jpg" class="card-img-top img-thumbnail" alt="logo">
-                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $value[0];?></h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-secondary">Go somewhere</a>
-                    </div>
-                </div>
+             <div class="card <?php if($value[3]==true){echo "bg-primary";}?>" style="width: 18rem;">
+               <img src="asset/images/logo.jpg" class="card-img-top bg-transparent" alt="...">
+                 <div class="card-body">
+                   <h5 class="card-title"><?php echo $value[0];?></h5>
+                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                   <a href="#" class="btn btn-dark">Go somewhere</a>
+                 </div>
+             </div>
             </div>
 
             <?php endforeach; ?>
-     
-         </div>
-     </div>
-    
 
-    <script src="asset/js/jquery-3-5.1.slim.min.js"></script>
-    <script src="asset/popper.min.js"></script>
-    <script src=bootstrap.min.js></script>
+        </div>
+    </div>
+
+    <!-- <script src="assets/JS/jquery-3.5.1.slim.min.js"></script> -->
+    <script src="asset/js/jquery-3-5-1.full.js"></script>
+    <script src="asset/js/popper.min.js"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+    <script src="asset/js/data.js"></script>
 </body>
 </html>
